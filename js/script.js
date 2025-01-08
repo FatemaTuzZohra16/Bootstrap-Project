175971fd629476d5acb4b7fb55fddfbd70e63328
FatemaTuzZohra16 <aneyrahman16@gmail.com>
2025-01-08 15:01:47 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$('.bannar_slider').slick({
+    arrows: false,
+    dots: true,
+});
+$('.reviewslider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: `<i class="fa-solid fa-angle-left leftarrow"></i>`,
+    nextArrow: `<i class="fa-solid fa-angle-right rightarrow"></i>`,
+
+    centerMode: true,
+    centerPadding: '0px',
+});
